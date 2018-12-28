58269a3f45e7354f6abc2f87783b554b259cb796
Johan Andersson <nilsjohanandersson@gmail.com>
2018-12-28 18:27:29 +0100

    Day 02

--- 2018/tests.lisp
+++ 2018/tests.lisp
@@ -5,3 +5,7 @@
 (deftest day01 ()
   (is (= 592 (aoc2018.day01::part1 (input-for 2018 1))))
   (is (= 241 (aoc2018.day01::part2 (input-for 2018 1)))))
+
+(deftest day02 ()
+  (is (= 6448 (aoc2018.day02::part1 (input-for 2018 2))))
+  (is (string= "evsialkqyiurohzpwucngttmf" (aoc2018.day02::part2 (input-for 2018 2)))))
--- package.lisp
+++ package.lisp
@@ -5,6 +5,7 @@
   (:export #:input-for #:lines #:trim-lf))
 
 (defpackage #:aoc2018.day01 (:use #:cl))
+(defpackage #:aoc2018.day02 (:use #:cl))
 
 (fiasco:define-test-package #:aoc2018.tests
   (:use #:aoc))

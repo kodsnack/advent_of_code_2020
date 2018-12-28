598b8b7f42754a6ee5573afabb0a5d6e4818d434
Johan Andersson <nilsjohanandersson@gmail.com>
2018-12-28 14:28:25 +0100

    Day 01

--- 2018/day01.lisp
+++ 2018/day01.lisp
@@ -2,6 +2,34 @@
 
 (in-package #:aoc2018.day01)
 
-(defun part1 (input))
+(defun numbers (strings)
+  (mapcar #'read-from-string strings))
 
-(defun part2 (input))
+(defun make-set ()
+  (let ((set (make-hash-table)))
+    (lambda (x)
+      (prog1 (gethash x set)
+        (setf (gethash x set) x)))))
+
+(defun make-device ()
+  (let ((frequencies (make-set))
+        (freq 0))
+    (funcall frequencies 0)
+    (lambda (change)
+      (funcall frequencies (incf freq change)))))
+
+(defun calibrate (device changes)
+  (loop for change in changes
+        for freq = (funcall device change)
+        until freq
+        finally (return freq)))
+
+(defun circular (list)
+  (setf (cdr (last list)) list)
+  list)
+
+(defun part1 (input)
+  (reduce #'+ (numbers (aoc:lines input))))
+
+(defun part2 (input)
+  (calibrate (make-device) (circular (numbers (aoc:lines input)))))
--- 2018/tests.lisp
+++ 2018/tests.lisp
@@ -3,5 +3,5 @@
 (in-package #:aoc2018.tests)
 
 (deftest day01 ()
-  (is (aoc2018.day01::part1 (input-for 2018 1)))
-  (is (aoc2018.day01::part2 (input-for 2018 1))))
+  (is (= 592 (aoc2018.day01::part1 (input-for 2018 1))))
+  (is (= 241 (aoc2018.day01::part2 (input-for 2018 1)))))

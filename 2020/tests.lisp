10e696b7be3cd72f4de4ebc8bc2b25a434904d36
Johan Andersson <nilsjohanandersson@gmail.com>
2020-12-01 21:36:50 +0100

    Prepare for 2020

---
+++ 2020/tests.lisp
@@ -0,0 +1,8 @@
+;;;; tests.lisp
+
+(in-package #:aoc2020.tests)
+
+(deftest day01 ()
+  (is (aoc2020.day01::part1 (input-for 2020 1)))
+  (is (aoc2020.day01::part1 (input-for 2020 1))))
+
--- package.lisp
+++ package.lisp
@@ -4,18 +4,7 @@
   (:use #:cl)
   (:export #:input-for #:lines #:trim-lf #:strip-cr))
 
-(defpackage #:aoc2018.day01 (:use #:cl))
-(defpackage #:aoc2018.day02 (:use #:cl))
-(defpackage #:aoc2018.day03 (:use #:cl))
-(defpackage #:aoc2018.day04 (:use #:cl))
-(defpackage #:aoc2018.day05 (:use #:cl))
-(defpackage #:aoc2018.day06 (:use #:cl))
-(defpackage #:aoc2018.day07 (:use #:cl) (:import-from #:alexandria #:compose #:curry))
-(defpackage #:aoc2018.day08 (:use #:cl))
-(defpackage #:aoc2018.day09 (:use #:cl))
-(defpackage #:aoc2018.day10 (:use #:cl) (:import-from #:alexandria #:curry))
-(defpackage #:aoc2018.day11 (:use #:cl))
-(defpackage #:aoc2018.day12 (:use #:cl))
+(defpackage #:aoc2020.day01 (:use #:cl))
 
-(fiasco:define-test-package #:aoc2018.tests
+(fiasco:define-test-package #:aoc2020.tests
   (:use #:aoc))

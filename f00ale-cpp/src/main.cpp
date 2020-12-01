aca57f5693d68e33dc0dac55516756b352ca3cb6
Arno Lepisk <arno@lepisk.se>
2020-12-01 18:04:28 +0100

    Add total time printout.

--- f00ale-cpp/src/main.cpp
+++ f00ale-cpp/src/main.cpp
@@ -10,6 +10,7 @@ int main() {
     std::vector<std::function<std::tuple<std::string,std::string>(std::istream&)>> problems
             { p01 };
 
+    std::chrono::microseconds total_time{0};
 
     for(size_t i = 0; i < problems.size(); i++)
     {
@@ -21,14 +22,17 @@ int main() {
             auto ret = problems[i](input);
             auto end = std::chrono::high_resolution_clock::now();
 
-            std::cout << std::setw(2) << num << ": " << std::setw(15) << std::get<0>(ret) <<
+            auto timeus = std::chrono::duration_cast<std::chrono::microseconds>(end-start);
+            total_time += timeus;
+            std::cout << std::right << std::setw(2) << num << ": " << std::setw(15) << std::get<0>(ret) <<
                     " " << std::setw(15) << std::get<1>(ret)
-                    << " " << std::setw(10) << std::chrono::duration_cast<std::chrono::microseconds>(end-start).count() << "us" << std::endl;
+                    << " " << std::setw(10) << timeus.count() << "us" << std::endl;
 
         } else {
             std::cout << "Problem opening " << filename << std::endl;
         }
     }
 
+    std::cout << std::left << std::setw(36) << "Total: " << std::right << std::setw(10) << total_time.count() << "us" << std::endl;
 
 }

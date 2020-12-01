fa6018fe54e44f93f89a2ef6c4c9a9c1d48513f1
william.io <william.io@MBP-de-Willy.local>
2020-12-02 00:44:17 +0100

    Day 1 Puzzle written in Ada 2012 On branch CaptainHaddock-Ada Changes to be committed: 	new file:   CaptainHaddock-Ada/Puzzle-01/Puzzle-01.dat 	new file:   CaptainHaddock-Ada/Puzzle-01/Puzzle_01.gpr 	new file:   CaptainHaddock-Ada/Puzzle-01/src/Puzzle_01_A.adb 	new file:   CaptainHaddock-Ada/Puzzle-01/src/Puzzle_01_B.adb 	new file:   CaptainHaddock-Ada/Puzzle-01/src/expenses.adb 	new file:   CaptainHaddock-Ada/Puzzle-01/src/expenses.ads

---
+++ CaptainHaddock-Ada/Puzzle-01/src/Puzzle_01_B.adb
@@ -0,0 +1,71 @@
+with Expenses; use Expenses;
+
+with Ada.Command_Line; use Ada.Command_Line;
+with Ada.text_IO; use Ada.text_IO;
+
+
+procedure Puzzle_01_B is
+
+    my_Expenses : Ordered_Expenses.Set;
+    index_A, index_B, index_C : Ordered_Expenses.Cursor;
+
+    Special_expense : natural := 2020;
+
+--  DAT_File_Name : String(1..2**15); -- 32_768
+    Missing_FileName : exception;
+
+    use Ordered_Expenses;
+begin
+
+    -- get the filename
+    if Argument_Count /= 1 then
+        raise Missing_FileName;
+    end if;
+
+    my_Expenses := get_expenses(Argument(1));
+
+    index_A := First(my_Expenses);
+    while Has_Element(index_A) loop
+        index_B:= Next(index_A);
+        while  Has_Element(index_B) loop
+            index_C:= Next(index_B);
+            while  Has_Element(index_C) loop
+                -- put(Standard_Error,"."); -- breadcum ...
+                if Element(index_A) + Element(index_B) + Element(index_C) = Special_expense then
+                    -- Found Special_expense !
+                    new_line;
+                    put(Expense'Image(Special_expense) &" = ");
+                    put(Expense'Image(Element(index_A)) &" + ");
+                    put(Expense'Image(Element(index_B)) &" + ");
+                    put(Expense'Image(Element(index_C)));
+                    new_line;
+                    -- Print-out the magic Number !
+                    put("Puzzle #1 Number = ");
+                    put(Expense'Image(Element(index_A) * Element(index_B) * Element(index_C)));
+                    new_line;
+                end if;
+                exit when Element(index_A)+Element(index_B)+Element(index_C) > Special_expense;
+                Next(index_C);
+            end loop;
+            Next(index_B);
+        end loop;
+        next(index_A);   
+    end loop;
+
+    set_Exit_Status(Success);
+
+exception
+    When Missing_FileName =>
+        put_line("usage: "& Command_Name & " expenses_file_name");
+        set_Exit_Status(Failure);
+    
+    when Status_Error =>
+        put_line(Standard_Error,"file '"&Argument(1)&"' not found!");
+        set_Exit_Status(Failure);
+        raise;
+  
+    when others => 
+        put_line(Standard_Error,"no results!");
+        set_Exit_Status(Failure);
+        raise;
+end Puzzle_01_B;

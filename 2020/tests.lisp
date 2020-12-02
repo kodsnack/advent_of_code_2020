7631cd9df28ceb297e21274105f8d085a7cee943
Johan Andersson <nilsjohanandersson@gmail.com>
2020-12-02 07:55:46 +0100

    Day 02

--- 2020/tests.lisp
+++ 2020/tests.lisp
@@ -6,3 +6,6 @@
   (is (= 633216 (aoc2020.day01::part1 (input-for 2020 01))))
   (is (= 68348924 (aoc2020.day01::part2 (input-for 2020 01)))))
 
+(deftest day02 ()
+  (is (= 517 (aoc2020.day02::part1 (input-for 2020 02))))
+  (is (= 284 (aoc2020.day02::part2 (input-for 2020 02)))))

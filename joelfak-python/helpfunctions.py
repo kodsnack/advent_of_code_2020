6de9d1f01ba1bba28f2c46f27ae05504b9280925
Joel Fåk <joel.faak@gmail.com>
2020-12-02 13:52:35 +0100

    Move timing to helpfunctions and update template

--- joelfak-python/helpfunctions.py
+++ joelfak-python/helpfunctions.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python3
 
+from functools import wraps
+from time import time
+import unittest
+
+def timing(f):
+    @wraps(f)
+    def wrap(*args, **kw):
+        ts = time()
+        result = f(*args, **kw)
+        te = time()
+        print('Execution of:%r took: %2.4f sec' % \
+          (f.__name__, te-ts))
+        return result
+    return wrap
+
 def readFile(filename):
     with open(filename) as file:
         while True:

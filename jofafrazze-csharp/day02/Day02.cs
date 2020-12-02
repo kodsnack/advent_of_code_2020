b26c8596cd1641237fe21e8362df35c7ecc1c3c4
jofafrazze <johan@hallonbrax.com>
2020-12-02 06:56:22 +0100

    Day 2

--- jofafrazze-csharp/day02/Day02.cs
+++ jofafrazze-csharp/day02/Day02.cs
@@ -2,12 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-
-using AdventOfCode;
-//using Position = AdventOfCode.GenericPosition2D<int>;
 
 namespace day02
 {
@@ -16,17 +11,79 @@ namespace day02
         readonly static string nsname = typeof(Day02).Namespace;
         readonly static string inputPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\..\" + nsname + "\\input.txt");
 
+        struct DbEntry
+        {
+            public int n1;
+            public int n2;
+            public char c;
+            public string data;
+        };
+
+        static List<DbEntry> ReadInput(string path)
+        {
+            StreamReader reader = File.OpenText(path);
+            List<DbEntry> list = new List<DbEntry>();
+            string line;
+            Regex parts = new Regex(@"^(\d+)-(\d+) ([a-z])\: ([a-z]+)");
+            while ((line = reader.ReadLine()) != null)
+            {
+                MatchCollection matches = parts.Matches(line);
+                if (matches.Count > 0)
+                {
+                    GroupCollection groups = matches[0].Groups;
+                    DbEntry d = new DbEntry();
+                    d.n1 = int.Parse(groups[1].Value);
+                    d.n2 = int.Parse(groups[2].Value);
+                    d.c = groups[3].Value[0];
+                    d.data = groups[4].Value;
+                    list.Add(d);
+                }
+            }
+            return list;
+        }
+
+        static List<DbEntry> ReadInput2(string path)
+        {
+            StreamReader reader = File.OpenText(path);
+            List<DbEntry> list = new List<DbEntry>();
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                DbEntry d = new DbEntry();
+                string[] s = line.Split(' ').ToArray();
+                string[] t = s[0].Split('-').ToArray();
+                d.n1 = int.Parse(t[0]);
+                d.n2 = int.Parse(t[1]);
+                d.c = s[1][0];
+                d.data = s[2];
+                list.Add(d);
+            }
+            return list;
+        }
+
         static Object PartA()
         {
-            List<int> input = ReadInputs.ReadInts(inputPath);
+            List<DbEntry> input = ReadInput(inputPath);
             int ans = 0;
+            foreach (DbEntry d in input)
+            {
+                int n = d.data.Count(x => x == d.c);
+                if (n >= d.n1 && n <= d.n2)
+                    ans++;
+            }
             Console.WriteLine("Part A: Result is {0}", ans);
             return ans;
         }
 
         static Object PartB()
         {
+            List<DbEntry> input = ReadInput(inputPath);
             int ans = 0;
+            foreach (DbEntry d in input)
+            {
+                if (d.data[d.n1 - 1] == d.c ^ d.data[d.n2 - 1] == d.c)
+                    ans++;
+            }
             Console.WriteLine("Part B: Result is {0}", ans);
             return ans;
         }
@@ -40,8 +97,8 @@ namespace day02
 
         public static bool MainTest()
         {
-            int a = 42;
-            int b = 4711;
+            int a = 493;
+            int b = 593;
             return (PartA().Equals(a)) && (PartB().Equals(b));
         }
     }

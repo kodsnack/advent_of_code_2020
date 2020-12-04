8619b3d29c2a9ebd31dc77909ee92210cb2c0afc
John Sterling <john.sterling@logicmonitor.com>
2020-12-04 08:12:35 +0100

    format

--- johnste-go/3/3a.go
+++ johnste-go/3/3a.go
@@ -12,14 +12,14 @@ func check(e error) {
 	}
 }
 
-func readFile(path string, process func(int,string) string) []string {
+func readFile(path string, process func(int, string) string) []string {
 	file, err := os.Open(path)
 	defer file.Close()
-	
+
 	check(err)
-		
+
 	result := []string{}
-	scanner := bufio.NewScanner(file)	
+	scanner := bufio.NewScanner(file)
 
 	for scanner.Scan() {
 		result = append(result, process(0, scanner.Text()))
@@ -28,50 +28,47 @@ func readFile(path string, process func(int,string) string) []string {
 	return result
 }
 
-
 func isTree(data []string, x int, y int, width int) bool {
-	return data[y][x % width] == '#'
+	return data[y][x%width] == '#'
 }
 
 type slope struct {
 	x, y int
 }
 
+func main() {
 
-func main() {	
-	
 	data := readFile("./data.txt", func(i int, text string) string {
 		return text
 	})
 
-	width := len(data[0])	
+	width := len(data[0])
 	trees := 0
 	x := 0
 
 	// 3A
-	for y, _ := range data {				
+	for y, _ := range data {
 		if isTree(data, x, y, width) {
 			trees += 1
 		}
 		x += 3
-	}	
+	}
 
 	fmt.Printf("3A Answer: %d\n", trees)
-
 
 	// 3B
 	slopes := []slope{
 		{1, 1},
-		{3, 1}, 
+		{3, 1},
 		{5, 1},
 		{7, 1},
 		{1, 2},
-	} 
-	
+	}
+
 	allSlopes := []int{}
 
 	for _, slope := range slopes {
-		trees := 0		
+		trees := 0
 		x := 0
 		y := 0
 		for y < len(data) {
@@ -81,7 +78,7 @@ func main() {
 			x += slope.x
 			y += slope.y
 		}
-		
+
 		allSlopes = append(allSlopes, trees)
 	}
 

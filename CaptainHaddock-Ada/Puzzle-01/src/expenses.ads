0af2955e607b5d6acccc4765603fbe932dbe6d55
william.io <william.io@MBP-de-Willy.local>
2020-12-04 17:13:18 +0100

    Added Author and License.

--- CaptainHaddock-Ada/Puzzle-01/src/expenses.ads
+++ CaptainHaddock-Ada/Puzzle-01/src/expenses.ads
@@ -1,3 +1,11 @@
+-- ------------------------------------------------
+-- Author : William J. FRANCK
+-- e-Mail : william@sterna.io
+--
+-- Initial creation date : 2020-12-01 
+-- ------------------------------------------------
+-- License : CC-BY-SA 
+-- ------------------------------------------------
 with Ada.Containers.Ordered_Sets;
 with Ada.Text_IO;
 

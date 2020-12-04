bf160ce99bb3dab7e4c5bc5abc337a4a4fc5992d
Tomas Kronvall <tomas@cernael.com>
2020-12-04 07:26:39 +0100

    days 3 and 4

---
+++ cernael-python/a.py
@@ -0,0 +1,9 @@
+def solve(lines):
+    pass
+
+if __name__ == '__main__':
+    lines = []
+    with open('.txt') as f:
+        for line in f.readlines():
+            lines.append(line)
+    print(solve(lines))

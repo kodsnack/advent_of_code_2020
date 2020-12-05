388a13b39c2184f2a384f8e6b49b8a95af78e8ea
jofafrazze <johan@hallonbrax.com>
2020-12-05 07:18:32 +0100

    Day 5 rewrite to try using the Set and Linq way

--- jofafrazze-csharp/day05/Day05.cs
+++ jofafrazze-csharp/day05/Day05.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-
+using System.Linq;
 using AdventOfCode;
 
 namespace day05
@@ -11,49 +11,44 @@ namespace day05
         readonly static string nsname = typeof(Day05).Namespace;
         readonly static string inputPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\..\" + nsname + "\\input.txt");
 
-        static int GetId(string s)
+        static HashSet<int> GetIds(List<string> list)
         {
-            int row = 0;
-            for (int i = 0; i < 7; i++)
+            HashSet<int> ids = new HashSet<int>();
+            foreach (string s in list)
             {
-                row <<= 1;
-                int bit = s[i] == 'F' ? 0 : 1;
-                row |= bit;
+                int row = 0;
+                for (int i = 0; i < 7; i++)
+                {
+                    row <<= 1;
+                    int bit = s[i] == 'F' ? 0 : 1;
+                    row |= bit;
+                }
+                int col = 0;
+                for (int i = 7; i < 10; i++)
+                {
+                    col <<= 1;
+                    int bit = s[i] == 'L' ? 0 : 1;
+                    col |= bit;
+                }
+                ids.Add(row * 8 + col);
             }
-            int col = 0;
-            for (int i = 7; i < 10; i++)
-            {
-                col <<= 1;
-                int bit = s[i] == 'L' ? 0 : 1;
-                col |= bit;
-            }
-            return row * 8 + col;
+            return ids;
         }
 
         static Object PartA()
         {
-            int ans = 0;
             List<string> input = ReadInputs.ReadStrings(inputPath);
-            foreach (string s in input)
-            {
-                int id = GetId(s);
-                if (id > ans)
-                    ans = id;
-            }
+            HashSet<int> ids = GetIds(input);
+            int ans = ids.Max();
             Console.WriteLine("Part A: Result is {0}", ans);
             return ans;
         }
 
         static Object PartB()
         {
-            int ans = 0;
             List<string> input = ReadInputs.ReadStrings(inputPath);
-            HashSet<int> ids = new HashSet<int>();
-            foreach (string s in input)
-                ids.Add(GetId(s));
-            foreach (int id in ids)
-                if (!ids.Contains(id + 1) && ids.Contains(id + 2))
-                    ans = id + 1;
+            HashSet<int> ids = GetIds(input);
+            int ans = ids.Where(x => !ids.Contains(x + 1) && ids.Contains(x + 2)).Min() + 1;
             Console.WriteLine("Part B: Result is {0}", ans);
             return ans;
         }

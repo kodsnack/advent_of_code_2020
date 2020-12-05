ea4f71c4a50bb3a81d5159f3755b6e51c6aaec90
Linus Mellberg <lmellberg@baffinbaynetworks.com>
2020-12-05 17:22:31 +0100

    Input download.

--- aelg-rust/src/lib.rs
+++ aelg-rust/src/lib.rs
@@ -15,25 +15,27 @@ pub fn solveday(
     day: u32,
     input: &[&str],
 ) -> Result<(Option<String>, Option<String>), error::AOCError> {
-    if day == 1 {
-        let r = day01::parse(input);
-        return r.map(day01::solve);
+    match day {
+        1 => {
+            let r = day01::parse(input);
+            r.map(day01::solve)
+        }
+        2 => {
+            let r = day02::parse(input);
+            r.map(day02::solve)
+        }
+        3 => {
+            let r = day03::parse(input);
+            r.map(day03::solve)
+        }
+        4 => {
+            let r = day04::parse(input);
+            r.map(day04::solve)
+        }
+        5 => {
+            let r = day05::parse(input);
+            r.map(day05::solve)
+        }
+        _ => Ok((Some("No solution for this day yet.".into()), None)),
     }
-    if day == 2 {
-        let r = day02::parse(input);
-        return r.map(day02::solve);
-    }
-    if day == 3 {
-        let r = day03::parse(input);
-        return r.map(day03::solve);
-    }
-    if day == 4 {
-        let r = day04::parse(input);
-        return r.map(day04::solve);
-    }
-    if day == 5 {
-        let r = day05::parse(input);
-        return r.map(day05::solve);
-    }
-    Ok((None, None))
 }

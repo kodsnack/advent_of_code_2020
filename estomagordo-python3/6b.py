c4d61a32e988b0e74168605a2ae4485c515d2975
Christofer Ohlsson <christofer.ohlsson@gmail.com>
2020-12-06 06:07:45 +0100

    first iter

---
+++ estomagordo-python3/6b.py
@@ -0,0 +1,40 @@
+
+from collections import Counter, defaultdict, deque
+from functools import reduce
+from heapq import heappop, heappush
+from itertools import combinations, permutations, product
+
+from helpers import distance, distance_sq, ints, manhattan, neighs, neighs_bounded
+
+
+def solve(qs):
+    count = 0
+
+    for group in qs:
+        s = set('abcdefghijklmnopqrstuvwxyz')
+
+        for g in group:
+            s &= set(g)
+
+        count += len(s)
+
+    return count
+
+if __name__ == '__main__':
+    qs = []
+    group = []
+
+    with open('6.txt') as f:
+        for line in f.readlines():
+            if not line.strip():
+                qs.append(group)
+                group = []
+            else:
+                group.append(line.rstrip())
+
+    if group:
+        qs.append(group)
+
+    print(solve(qs))
+
+# 1646

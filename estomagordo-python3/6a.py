625a68308f7bddf8d2a14936b421a578490dbeaf
Christofer Ohlsson <christofer.ohlsson@gmail.com>
2020-12-06 06:24:24 +0100

    bit more functional

--- estomagordo-python3/6a.py
+++ estomagordo-python3/6a.py
@@ -7,34 +7,19 @@ from itertools import combinations, permutations, product
 from helpers import distance, distance_sq, grouped_lines, ints, manhattan, neighs, neighs_bounded
 
 
-def solve(qs):
-    count = 0
-
-    for group in qs:
-        s = set()
-
-        for g in group:
-            s |= set(g)
-
-        count += len(s)
-
-    return count
+def solve(groups):
+    return sum(
+                len(
+                    reduce(
+                        lambda a,b: a|b,
+                        map(set, group),
+                        set ()
+                    )
+                )
+                for group in groups
+            )
 
 if __name__ == '__main__':
-    qs = []
-    group = []
-
     with open('6.txt') as f:
-        for line in f.readlines():
-            if not line.strip():
-                qs.append(group)
-                group = []
-            else:
-                group.append(line.rstrip())
-
-    if group:
-        qs.append(group)
-
-    print(solve(qs))
-
-# 1646
+        groups = grouped_lines(f.readlines())
+        print(solve(groups))

331105fc8b40c6cc482cd64b3d9c0b6157635430
Arno Lepisk <arno@lepisk.se>
2020-12-07 06:47:48 +0100

    Day 7

--- f00ale-cpp/src/main.cpp
+++ f00ale-cpp/src/main.cpp
@@ -8,7 +8,7 @@
 
 int main() {
     std::vector<std::function<std::tuple<std::string,std::string>(const std::string&)>> problems
-            { p01, p02, p03, p04, p05, p06 };
+            { p01, p02, p03, p04, p05, p06, p07 };
 
     std::chrono::microseconds total_time{0};
 

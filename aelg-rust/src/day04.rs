4966a5826e83d4cd087651d65f3acaca7c6b4d5b
Linus Mellberg <lmellberg@baffinbaynetworks.com>
2020-12-07 19:08:49 +0100

    Nicer day4

--- aelg-rust/src/day04.rs
+++ aelg-rust/src/day04.rs
@@ -1,5 +1,6 @@
 use crate::error;
 use crate::parser;
+use crate::parser::IResult;
 
 #[derive(Debug, Clone)]
 pub struct Passport {
@@ -13,9 +14,9 @@ pub struct Passport {
   cid: Option<String>,
 }
 
-type Data = Vec<Option<Passport>>;
+type Data = Vec<Passport>;
 
-fn parser(input: &str) -> Result<Data, error::AOCError> {
+fn parser(input: &str) -> IResult<&str, Vec<Option<Passport>>> {
   use parser::*;
   pub fn field(field_name: &'static str) -> impl Fn(&str) -> IResult<&str, &str> {
     move |input: &str| {
@@ -52,80 +53,79 @@ fn parser(input: &str) -> Result<Data, error::AOCError> {
       }),
     ))
   }
+  let invalid_passport = map(many_till(anychar, tag("\n\n")), |(_, _)| None);
 
-  fn invalid_passport(input: &str) -> IResult<&str, Option<Passport>> {
-    let (input, _) = take_until("\n\n")(input)?;
-    let (input, _) = tag("\n\n")(input)?;
-    Ok((input, None))
-  }
+  let passport = alt((passport_cid, invalid_passport));
 
-  fn passport(input: &str) -> IResult<&str, Option<Passport>> {
-    let (input, passport) = alt((passport_cid, invalid_passport))(input)?;
-    Ok((input, passport))
-  }
-
-  match many1(passport)(input) {
-    Ok((_, s)) => Ok(s),
-    Err(e) => Err(format!("failed: {}", e).as_str().into()),
-  }
+  many1(passport)(input)
 }
 
-pub fn parse<'a>(input: &'a [&str]) -> Result<String, error::AOCError> {
+pub fn parse<'a>(input: &'a [&str]) -> Result<Data, error::AOCError> {
   let mut s = input.join("\n");
   s.push('\n');
   s.push('\n');
-  Ok(s)
-}
 
-fn solve1(s: String) -> Option<String> {
-  if let Ok(v) = parser(&s) {
-    return Some(format!("{}", v.into_iter().filter_map(|x| x).count()));
+  match parser(s.as_str()) {
+    Ok(("", data)) => Ok(data.into_iter().filter_map(|x| x).collect()),
+    Ok((input, _)) => Err(
+      format!("Couldn't parse, stopped near: {}", input)
+        .as_str()
+        .into(),
+    ),
+    Err(e) => Err(
+      format!("Couldn't parse {}\nerror: {}", s, e)
+        .as_str()
+        .into(),
+    ),
   }
-  None
 }
 
-fn valid_passport(p: Passport) -> Option<()> {
+fn solve1(s: &[Passport]) -> Option<String> {
+  Some(format!("{}", s.len()))
+}
+
+fn valid_passport(p: &&Passport) -> bool {
   use parser::*;
   match integer::<u32>(p.byr.as_str()) {
     Ok(("", y)) => {
       if y < 1920 || y > 2002 {
-        return None;
+        return false;
       }
     }
-    _ => return None,
+    _ => return false,
   }
 
   match integer::<u32>(p.iyr.as_str()) {
     Ok(("", y)) => {
       if y < 2010 || y > 2020 {
-        return None;
+        return false;
       }
     }
-    _ => return None,
+    _ => return false,
   }
 
   match integer::<u32>(p.eyr.as_str()) {
     Ok(("", y)) => {
       if y < 2020 || y > 2030 {
-        return None;
+        return false;
       }
     }
-    _ => return None,
+    _ => return false,
   }
 
   match tuple((integer::<u32>, alt((tag("cm"), tag("in")))))(p.hgt.as_str()) {
     Ok(("", (h, unit))) => {
       if unit == "cm" && (h < 150 || h > 193) || unit == "in" && (h < 59 || h > 76) {
-        return None;
+        return false;
       }
     }
-    _ => return None,
+    _ => return false,
   }
 
   match tuple::<_, _, (), _>((tag("#"), many_m_n(6, 6, one_of("0123456789abcdef"))))(p.hcl.as_str())
   {
     Ok(("", _)) => (),
-    _ => return None,
+    _ => return false,
   }
 
   match alt::<_, _, (), _>((
@@ -139,32 +139,22 @@ fn valid_passport(p: Passport) -> Option<()> {
   ))(p.ecl.as_str())
   {
     Ok(("", _)) => (),
-    _ => return None,
+    _ => return false,
   }
 
   match many_m_n::<_, _, (), _>(9, 9, one_of("0123456789"))(p.pid.as_str()) {
     Ok(("", _)) => (),
-    _ => return None,
+    _ => return false,
   }
-
-  Some(())
+  true
 }
 
-fn solve2(s: String) -> Option<String> {
-  match parser(&s) {
-    Ok(v) => Some(format!(
-      "{}",
-      v.into_iter()
-        .filter_map(|x| x.map(valid_passport))
-        .filter_map(|x| x)
-        .count()
-    )),
-    Err(e) => Some(format!("{}", e)),
-  }
+fn solve2(s: &[Passport]) -> Option<String> {
+  Some(format!("{}", s.iter().filter(valid_passport).count()))
 }
 
-pub fn solve(v: String) -> (Option<String>, Option<String>) {
-  (solve1(v.clone()), solve2(v))
+pub fn solve(v: Data) -> (Option<String>, Option<String>) {
+  (solve1(&v), solve2(&v))
 }
 
 #[cfg(test)]
@@ -189,13 +179,14 @@ iyr:2011 ecl:brn hgt:59in
 
   #[test]
   fn test_example1() {
-    let result = parse(&input_from_str(EXAMPLE)).map(solve1).unwrap();
+    let parsed = parse(&input_from_str(EXAMPLE)).unwrap();
+    let result = solve1(&parsed);
     assert_eq!(result, Some("2".into()))
   }
-
   #[test]
   fn test_example2() {
-    let result = parse(&input_from_str(EXAMPLE)).map(solve2).unwrap();
+    let parsed = parse(&input_from_str(EXAMPLE)).unwrap();
+    let result = solve2(&parsed);
     assert_eq!(result, Some("2".into()))
   }
 }

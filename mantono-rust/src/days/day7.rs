23dbb7e22cb5416fca8abedd4bd8341c4ec33d9f
Anton Österberg <anton.osterberg@gmail.com>
2020-12-08 00:55:20 +0100

    Day 7, part 2. Close?

--- mantono-rust/src/days/day7.rs
+++ mantono-rust/src/days/day7.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, sync::Arc};
 
 use regex::Regex;
 
@@ -41,8 +41,7 @@ use regex::Regex;
 pub fn first(input: String) -> String {
     let bags = input
         .lines()
-        .map(|line: &str| BagMapping::from(line))
-        .map(|bg| (bg.bag_type, bg.content))
+        .map(|line: &str| map_bags(line))
         .collect::<HashMap<String, Vec<BagContent>>>();
 
     count(bags).to_string()
@@ -69,31 +68,58 @@ fn can_hold_golden_bag(bag: &str, bags: &HashMap<String, Vec<BagContent>>) -> bo
 }
 
 pub fn second(input: String) -> String {
-    input
+    let bags = input
+        .lines()
+        .filter(|line| !line.is_empty())
+        .map(|line: &str| map_bags(line))
+        .collect::<HashMap<String, Vec<BagContent>>>();
+
+    (rec_explore(&bags, "shiny gold")).to_string()
 }
 
-#[derive(Debug)]
-struct BagMapping {
-    pub bag_type: String,
-    pub content: Vec<BagContent>,
+// shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+// dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+// vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+// faded blue bags contain no other bags.
+// dotted black bags contain no other bags.
+fn rec_explore(bags: &HashMap<String, Vec<BagContent>>, bag: &str) -> usize {
+    let next: &Vec<BagContent> = match bags.get(bag) {
+        None => {
+            println!("{} return => {}", bag, 1);
+            return 1;
+        }
+        Some(next) => next,
+    };
+
+    if next.is_empty() {
+        println!("{} return => {}", bag, 1);
+        return 1;
+    }
+
+    let sum: usize = next
+        .iter()
+        .map(|c| rec_explore(bags, &c.bag_type) * c.size())
+        .sum();
+
+    let ret = sum + 1;
+    println!("{} return => {}", bag, ret);
+    ret
 }
 
 lazy_static::lazy_static! {
     static ref SPLIT: Regex = Regex::new("( bags contain | bags?[,.] ?)").unwrap();
 }
 
-impl BagMapping {
-    pub fn from(line: &str) -> BagMapping {
-        let mut parts = SPLIT.split(line);
-        let bag_type: String = parts.next().unwrap().to_string();
+fn map_bags(line: &str) -> (String, Vec<BagContent>) {
+    let mut parts = SPLIT.split(line);
+    let bag_type: String = parts.next().unwrap().to_string();
 
-        let content: Vec<BagContent> = parts
-            .filter(|bag| bag != &"no other")
-            .filter_map(|bag: &str| BagContent::from(bag))
-            .collect::<Vec<BagContent>>();
+    let content: Vec<BagContent> = parts
+        .filter(|bag| bag != &"no other")
+        .filter_map(|bag: &str| BagContent::from(bag))
+        .collect::<Vec<BagContent>>();
 
-        BagMapping { bag_type, content }
-    }
+    (bag_type, content)
 }
 
 #[derive(Debug)]
@@ -112,11 +138,15 @@ impl BagContent {
         let bag_type: String = chars.skip(1).collect::<String>();
         Some(BagContent { quantity, bag_type })
     }
+
+    fn size(&self) -> usize {
+        self.quantity as usize
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::first;
+    use super::{first, second};
 
     #[test]
     fn test_part1() {
@@ -133,5 +163,29 @@ dotted black bags contain no other bags.
 ";
 
         assert_eq!("4", &first(input.to_string()));
+    }
+
+    #[test]
+    fn test_part2() {
+        let input: &str = r"
+shiny gold bags contain 2 dark red bags.
+dark red bags contain 2 dark orange bags.
+dark orange bags contain 2 dark yellow bags.
+dark yellow bags contain 2 dark green bags.
+dark green bags contain 2 dark blue bags.
+dark blue bags contain 2 dark violet bags.
+dark violet bags contain no other bags.
+";
+        assert_eq!("126", &second(input.to_string()));
+    }
+
+    #[test]
+    fn test_part2b() {
+        let input: &str = r"
+shiny gold bags contain 2 dark red bags.
+dark red bags contain 1 dark orange bag.
+dark orange bags contain no other bags.
+";
+        assert_eq!("4", &second(input.to_string()));
     }
 }

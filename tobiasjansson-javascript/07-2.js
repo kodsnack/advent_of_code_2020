5564685e8842733bdf7f026b4bb9a50f1090f5ac
Tobias Jansson <Tobias.Jansson@prevas.se>
2020-12-07 15:57:30 +0100

    Dag 7 del 1 och 2

---
+++ tobiasjansson-javascript/07-2.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const args = process.argv.slice(2);
+
+const data = fs.readFileSync(args[0], 'utf8');
+
+let lines = data.split('\n');
+
+let bags = [];
+lines.forEach(line => {
+    let bag = line.match(/(.*) bags contain (.*)/);
+    let top = bag[1];
+    let contain = bag[2];
+
+    subbags(top, contain);
+});
+
+console.log(countshiny(0, 'shiny gold'));
+
+function subbags(top, subbags) {
+    subbags.split(',').forEach(x => {
+        let bag = x.match(/\s?(\d+) (.*) bag\.*/);
+        if (bags[top] == undefined) {
+            bags[top] = [];
+        }
+        if (bag != null) {
+            let amount = bag[1];
+            let contain = bag[2];
+            bags[top].push({color:contain, amount:parseInt(amount)});
+        } else {
+            bags[top].push({amount:0});           
+        }
+    });
+}
+
+function countshiny(count, bag) {
+    if (bags[bag]) {
+        bags[bag].forEach(b => {
+           count += b.amount;
+           count += b.amount * countshiny(0, b.color);
+        });
+    }
+    return count;
+}

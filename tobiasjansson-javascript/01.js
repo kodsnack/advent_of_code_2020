6f81852f985de973b86da045df233922ce2ae8ec
Tobias Jansson <Tobias.Jansson@prevas.se>
2020-12-11 11:06:37 +0100

    Dag 9,10, 11

--- tobiasjansson-javascript/01.js
+++ tobiasjansson-javascript/01.js
@@ -3,22 +3,55 @@ const args = process.argv.slice(2);
 
 const data = fs.readFileSync(args[0], 'utf8');
 
-let lines = data.split('\n');
+let lines = data.split('\n').map(x => parseInt(x));
 
-for (var i = 0; i < lines.length; i++) {
-    for (var k = 0; k < lines.length; k++) {
-        if (k != i) {
-            for (var j = 0; j < lines.length; j++) {
-                if (i != j && k != j) {
-                    let v1 = lines[i];
-                    let v2 = lines[j];
-                    let v3 = lines[k];
-                    if (parseInt(v1) + parseInt(v2) + parseInt(v3) == 2020) {
-                        console.log(v1, v2, v3, v1 * v2 * v3);
-                        return;
+// Min funktion
+console.log(hittatre(lines));
+
+// Tips från AoC att använda 3SUM
+console.log(sum3(lines));
+
+function hittatre(data) {
+    for (var i = 0; i < data.length; i++) {
+        for (var k = 0; k < data.length; k++) {
+            if (k != i) {
+                for (var j = 0; j < data.length; j++) {
+                    if (i != j && k != j) {
+                        let v1 = data[i];
+                        let v2 = data[j];
+                        let v3 = data[k];
+                        if (v1 + v2 + v3 == 2020) {
+                            return v1 * v2 * v3;
+                        }
                     }
                 }
             }
         }
     }
-};
+}
+
+// vill lära mig något
+// https://en.wikipedia.org/wiki/3SUM
+
+function sum3(data) {
+    let S = data.sort();
+    let n = data.length;
+    let a, b, c, start, end;
+    for(var i=0;i < n - 2 ; i++) {
+        a = S[i];
+        start = i + 1;
+        end = n - 1;
+        while(start < end) {
+            b = S[start];
+            c = S[end];
+            let sum = Math.round((a - (2020/3)) + (b - (2020/3)) + (c - (2020/3)));
+            if(sum == 0) {
+                return a*b*c;
+            } else if(sum > 0 ) {
+                end -= 1;
+            } else {
+                start += 1;
+            }
+        }
+    }
+}

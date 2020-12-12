a4947b7b927533d2ff062a90f835013668ecf5d1
Anton Österberg <anton.osterberg@gmail.com>
2020-12-12 23:16:53 +0100

    Day 11, better attempt, still not there

--- mantono-rust/src/days/day11.rs
+++ mantono-rust/src/days/day11.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 /// All decisions are based on the number of occupied seats adjacent to a given seat
 /// (one of the eight positions immediately up, down, left, right, or diagonal from the seat).
 /// The following rules are applied to every seat simultaneously:
@@ -8,77 +10,103 @@
 ///
 /// Floor (.) never changes; seats don't move, and nobody sits on the floor.
 pub fn first(input: String) -> String {
-    let vec: Vec<Pos> = input
+    let grid: Vec<Pos> = input
         .lines()
         .map(|line| line.chars())
         .flatten()
         .map(Pos::from)
         .collect::<Vec<Pos>>();
 
-    let area: Grid<Pos> = Grid::from(vec, 97).unwrap();
-
-    loop {
-        let updated_area: &Vec<Pos> = &area
-            .map(|(x, y)| {
-                let occ_seats: usize = occupied_seats(&area, x, y);
-                area.get(x, y).unwrap().update(occ_seats)
-            })
-            .collect::<Vec<Pos>>();
-    }
-
-    "".to_string()
+    update(grid, 0).to_string()
 }
 
-fn update_area(mut area: Grid<Pos>) -> usize {
-    let mut changes: usize = 0;
-    let area_next = area
-        .map(|(x, y)| {
-            let occ_seats: usize = occupied_seats(&area, x, y);
-            let seat_before: &Pos = area.get(x, y).unwrap();
-            let seat_after: Pos = seat_before.update(occ_seats);
-            if *seat_before != seat_after {
-                changes += 1
-            };
-            seat_after
+const WIDTH: usize = 97;
+
+fn update(grid: Vec<Pos>, occupied: usize) -> usize {
+    let grid_now = grid
+        .iter()
+        .enumerate()
+        .inspect(|(i, pos)| {
+            if i % WIDTH == WIDTH - 1 {
+                print!("{}\n", pos)
+            } else {
+                print!("{}", pos)
+            }
         })
+        .map(|(i, pos)| update_pos(&grid, i, pos))
         .collect::<Vec<Pos>>();
 
-    if changes == 0 {
-        area.reset_iter();
-        area.filter(|(x, y)| area.get(*x, *y).unwrap().is_occupied())
-            .count()
+    let occupied_now: usize = count_occupied(&grid_now);
+
+    println!("\n");
+
+    if occupied == occupied_now {
+        occupied_now
     } else {
-        let grid = Grid::from(area_next, 97).unwrap();
-        update_area(grid)
+        //println!("Occupied seats: {}", occupied_now);
+        update(grid_now, occupied_now)
     }
 }
 
-fn occupied_seats(area: &Grid<Pos>, x: usize, y: usize) -> usize {
-    area.neighbours(x, y)
+fn update_pos(grid: &Vec<Pos>, i: usize, pos: &Pos) -> Pos {
+    if let Pos::Floor = pos {
+        return *pos;
+    }
+
+    std::thread::sleep(std::time::Duration::from_millis(8));
+
+    let neighbours: Vec<usize> = neighbours(i);
+    let adj_occ = neighbours
         .iter()
-        .filter(|p| p.is_occupied())
-        .count()
+        .map(|index: &usize| grid.get(*index).unwrap())
+        .filter(|p: &&Pos| p.is_occupied())
+        .count();
+
+    let new = pos.update(adj_occ);
+    //println!("{:?} [{}] => {} => {:?}", pos, i, adj_occ, new);
+    new
+}
+///    C1   C2    C3
+/// |-----------------|
+/// | -98 | -97 | -96 | R1
+/// | -1  |  X  | +1  | R2
+/// | +96 | +97 | +98 | R3
+/// |-----------------|
+fn neighbours(i: usize) -> Vec<usize> {
+    let (x, y) = coord(i);
+    let x_range = (x.max(1) - 1)..=(x + 1);
+    let y_range = (y.max(1) - 1)..=(y + 1);
+
+    //print!("\n{} => ", i);
+
+    vec![
+        i.checked_sub(WIDTH + 1),
+        i.checked_sub(WIDTH),
+        i.checked_sub(WIDTH - 1),
+        i.checked_sub(1),
+        Some(i + 1),
+        Some(i + WIDTH - 1),
+        Some(i + WIDTH),
+        Some(i + WIDTH + 1),
+    ]
+    .iter()
+    .filter_map(|x| *x)
+    .filter(|idx| {
+        let (x_i, y_i) = coord(*idx);
+        x_range.contains(&x_i) && y_range.contains(&y_i)
+    })
+    //.inspect(|n| print!("{} ", n))
+    .collect::<Vec<usize>>()
 }
 
-struct Grid<T: Sized> {
-    elements: Vec<T>,
-    width: usize,
-    counter: usize,
+fn coord(i: usize) -> (usize, usize) {
+    let x = i % WIDTH;
+    let y = i / WIDTH;
+    (x, y)
 }
 
-impl<T: Sized> Iterator for Grid<T> {
-    type Item = (usize, usize);
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.counter >= self.elements.len() {
-            None
-        } else {
-            let x = self.counter % self.width;
-            let y = self.counter / self.width;
-            self.counter += 1;
-            Some((x, y))
-        }
-    }
+fn count_occupied(grid: &Vec<Pos>) -> usize {
+    grid.iter().filter(|p| p.is_occupied()).count()
 }
 
 #[derive(Debug, Copy, Clone, Ord, PartialOrd, Eq, PartialEq)]
@@ -86,6 +114,17 @@ enum Pos {
     Floor,
     Empty,
     Occupied,
+}
+
+impl Display for Pos {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let c: char = match self {
+            Pos::Floor => '.',
+            Pos::Empty => 'L',
+            Pos::Occupied => '#',
+        };
+        write!(f, "{}", c)
+    }
 }
 
 impl Pos {
@@ -111,68 +150,6 @@ impl Pos {
             Pos::Occupied => true,
             _ => false,
         }
-    }
-}
-
-impl<T: Sized> Grid<T> {
-    pub fn from(elements: Vec<T>, width: usize) -> Result<Grid<T>, String> {
-        if elements.len() % width != 0 {
-            Err(format!(
-                "Cannot create Grid with width {} from vector of length {}",
-                width,
-                elements.len()
-            ))
-        } else {
-            Ok(Grid {
-                elements,
-                width,
-                counter: 0,
-            })
-        }
-    }
-
-    pub fn new(width: usize, height: usize) -> Grid<T> {
-        let vec: Vec<T> = Vec::with_capacity(width * height);
-        Grid {
-            elements: vec,
-            width,
-            counter: 0,
-        }
-    }
-
-    pub fn get<'a>(&'a self, x: usize, y: usize) -> Option<&'a T> {
-        let i: usize = x + y * self.width;
-        self.elements.get(i)
-    }
-
-    pub fn reset_iter(&mut self) {
-        self.counter = 0;
-    }
-
-    pub fn neighbours<'a>(&'a self, x: usize, y: usize) -> Vec<&'a T> {
-        let n0 = (x - 1, y - 1);
-        let n1 = (x, y - 1);
-        let n2 = (x + 1, y - 1);
-
-        let n3 = (x - 1, y);
-        let n4 = (x + 1, y);
-
-        let n5 = (x - 1, y + 1);
-        let n6 = (x, y + 1);
-        let n7 = (x + 1, y + 1);
-
-        let pos: Vec<(usize, usize)> = vec![n0, n1, n2, n3, n4, n5, n6, n7];
-        let x_range = 0..self.width;
-        let y_range = 0..self.height();
-
-        pos.iter()
-            .filter(|(x, y)| x_range.contains(x) && y_range.contains(y))
-            .filter_map(|(x, y)| self.get(*x, *y))
-            .collect::<Vec<&'a T>>()
-    }
-
-    fn height(&self) -> usize {
-        self.elements.len() / self.width
     }
 }
 

f63e6ad71c03682a6c14d4470f904801bf51ac34
jofafrazze <johan@hallonbrax.com>
2020-12-17 09:15:17 +0100

    Day 17

--- jofafrazze-csharp/day17/Day17.cs
+++ jofafrazze-csharp/day17/Day17.cs
@@ -1,13 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-
 using AdventOfCode;
-using Position = AdventOfCode.GenericPosition3D<int>;
 
 namespace day17
 {
@@ -16,27 +10,34 @@ namespace day17
         readonly static string nsname = typeof(Day17).Namespace;
         readonly static string inputPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\..\" + nsname + "\\input.txt");
 
-        static Map3D StepMap(Map3D m)
+        static HashSet<(int, int, int)> GetNeighbours3D((int x, int y, int z) p)
         {
-            int w = m.width;
-            int h = m.height;
-            int d = m.depth;
-            Map3D mNext = new Map3D(m);
-            for (int z = 0; z < d; z++)
+            var m = new HashSet<(int, int, int)>();
+            int[] offs = new int[] { -1, 0, 1 };
+            foreach (int i in offs)
+                foreach (int j in offs)
+                    foreach (int k in offs)
+                        if (i != 0 || j != 0 || k != 0)
+                            m.Add((p.x + i, p.y + j, p.z + k));
+            return m;
+        }
+
+        static HashSet<(int, int, int)> StepMap3D(HashSet<(int x, int y, int z)> m)
+        {
+            var mNext = new HashSet<(int, int, int)>(m);
+            HashSet<(int x, int y, int z)> mNeigh = new HashSet<(int x, int y, int z)>(m);
+            foreach (var p in m)
+                mNeigh.UnionWith(GetNeighbours3D(p));
+            foreach (var p in mNeigh)
             {
-                for (int y = 0; y < h; y++)
-                {
-                    for (int x = 0; x < w; x++)
-                    {
-                        var p = new Position(x, y, z);
-                        var c = m[p];
-                        int n = CoordsRCD.directions26.Where(q => m.HasPosition(p + q) && m[p + q] == '#').Count();
-                        if (c == '#' && (n < 2 || n > 3))
-                            mNext[p] = '.';
-                        if (c != '#' && n == 3)
-                            mNext[p] = '#';
-                    }
-                }
+                int n = 0;
+                foreach (var q in GetNeighbours3D(p))
+                    if (m.Contains(q))
+                        n++;
+                if (m.Contains(p) && (n < 2 || n > 3))
+                    mNext.Remove(p);
+                if (!m.Contains(p) && n == 3)
+                    mNext.Add(p);
             }
             return mNext;
         }
@@ -44,20 +45,20 @@ namespace day17
         static Object PartA()
         {
             var input = ReadIndata.Strings(inputPath);
-            int k = input.Count + 2 * 6;
-            Map3D m = Map3D.BuildZ0(k, input);
+            var m = new HashSet<(int x, int y, int z)>();
+            int k = input.Count;
+            for (int y = 0; y < k; y++)
+                for (int x = 0; x < k; x++)
+                    if (input[y][x] == '#')
+                        m.Add((x, y, 0));
             for (int i = 0; i < 6; i++)
-            {
-                //Console.WriteLine("Iteration {0}:", i);
-                //m.Print();
-                m = StepMap(m);
-            }
-            int ans = m.data.Cast<char>().Where(x => x == '#').Count();
+                m = StepMap3D(m);
+            int ans = m.Count;
             Console.WriteLine("Part A: Result is {0}", ans);
             return ans;
         }
 
-        static HashSet<(int, int, int, int)> Get80Neighbours((int x, int y, int z, int w) p)
+        static HashSet<(int, int, int, int)> GetNeighbours4D((int x, int y, int z, int w) p)
         {
             var m = new HashSet<(int, int, int, int)>();
             int[] offs = new int[] { -1, 0, 1 };
@@ -69,23 +70,19 @@ namespace day17
                                 m.Add((p.x + i, p.y + j, p.z + k, p.w + l));
             return m;
         }
-        static HashSet<(int, int, int, int)> StepHashMap(HashSet<(int x, int y, int z, int w)> m)
+
+        static HashSet<(int, int, int, int)> StepMap4D(HashSet<(int x, int y, int z, int w)> m)
         {
-            int[] offs = new int[] { -1, 0, 1 };
             var mNext = new HashSet<(int, int, int, int)>(m);
             HashSet<(int x, int y, int z, int w)> mNeigh = new HashSet<(int x, int y, int z, int w)>(m);
             foreach (var p in m)
-                mNeigh.UnionWith(Get80Neighbours(p));
+                mNeigh.UnionWith(GetNeighbours4D(p));
             foreach (var p in mNeigh)
             {
                 int n = 0;
-                foreach (int i in offs)
-                    foreach (int j in offs)
-                        foreach (int k in offs)
-                            foreach (int l in offs)
-                                if (i != 0 || j != 0 || k != 0 || l != 0)
-                                    if (m.Contains((p.x + i, p.y + j, p.z + k, p.w + l)))
-                                        n++;
+                foreach (var q in GetNeighbours4D(p))
+                    if (m.Contains(q))
+                        n++;
                 if (m.Contains(p) && (n < 2 || n > 3))
                     mNext.Remove(p);
                 if (!m.Contains(p) && n == 3)
@@ -103,12 +100,8 @@ namespace day17
                 for (int x = 0; x < k; x++)
                     if (input[y][x] == '#')
                         m.Add((x, y, 0, 0));
-            Console.WriteLine("Hypercubes: {0}", m.Count);
             for (int i = 0; i < 6; i++)
-            {
-                m = StepHashMap(m);
-                Console.WriteLine("Hypercubes {0}: {1}", i, m.Count);
-            }
+                m = StepMap4D(m);
             int ans = m.Count;
             Console.WriteLine("Part B: Result is {0}", ans);
             return ans;

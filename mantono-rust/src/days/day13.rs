a09f8026e53774dac2e9b54bc71664c584b1f236
Anton Österberg <anton.osterberg@gmail.com>
2020-12-19 01:43:02 +0100

    Working solution for day 13 part 2, BUT it gives a stack overflow....

--- mantono-rust/src/days/day13.rs
+++ mantono-rust/src/days/day13.rs
@@ -32,32 +32,39 @@ pub fn second(input: String) -> String {
         .filter_map(|(index, id)| BusLine::from(index, id).ok())
         .collect::<Vec<BusLine>>();
 
-    let mut i: usize = 0;
+    let last_bus: &BusLine = bus_lines.iter().max_by(|b0, b1| b0.id.cmp(&b1.id)).unwrap();
+    let max_id: u64 = last_bus.id;
+    let mut timestamp: u64 = 0;
+    let mut inc: u64 = 1;
+    let mut size: usize = 2;
 
     loop {
-        i += 1;
-        if i % 1_000_000_000 == 0 {
-            println!();
-            for b in &bus_lines {
-                println!("{} => {}", b.id, b.abs_timestamp());
-            }
-        }
+        let lines = &bus_lines[0..size];
+        timestamp = next_timestamp(lines, timestamp, inc);
+        inc = lines.iter().map(|b| b.id).product();
 
-        if are_aligned(&bus_lines) {
+        if lines.len() == bus_lines.len() {
             break;
         }
 
-        &bus_lines.iter_mut().min().unwrap().next();
+        size += 1;
     }
 
-    bus_lines.iter().min().unwrap().abs_timestamp().to_string()
+    timestamp.to_string()
+}
+
+fn next_timestamp(busses: &[BusLine], timestamp: u64, increment: u64) -> u64 {
+    if busses.iter().all(|b| b.time_fits(timestamp)) {
+        timestamp
+    } else {
+        next_timestamp(busses, timestamp + increment, increment)
+    }
 }
 
 #[derive(Debug)]
 struct BusLine {
     pub id: u64,
     pub slot: u64,
-    dep: u64,
 }
 
 impl BusLine {
@@ -66,64 +73,22 @@ impl BusLine {
         Ok(BusLine {
             id,
             slot: index as u64,
-            dep: 1,
         })
     }
 
-    pub fn abs_timestamp(&self) -> u64 {
-        self.id * self.dep
+    pub fn departs_at(&self, timestamp: u64) -> bool {
+        let offset = timestamp % self.id;
+        offset == 0
     }
 
-    #[inline]
-    pub fn adj_timestamp(&self) -> u64 {
-        self.id * self.dep - self.slot
+    pub fn time_fits(&self, timestamp: u64) -> bool {
+        self.departs_at(timestamp + self.slot)
     }
-}
-
-impl Iterator for BusLine {
-    type Item = u64;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        self.dep += 1;
-        Some(self.adj_timestamp())
-    }
-}
-
-impl Ord for BusLine {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        if self.abs_timestamp() < other.abs_timestamp() {
-            std::cmp::Ordering::Less
-        } else if self.abs_timestamp() > other.abs_timestamp() {
-            std::cmp::Ordering::Greater
-        } else {
-            std::cmp::Ordering::Equal
-        }
-    }
-}
-
-impl PartialOrd for BusLine {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl Eq for BusLine {}
-
-impl PartialEq for BusLine {
-    fn eq(&self, other: &Self) -> bool {
-        self.id == other.id
-    }
-}
-
-#[inline(always)]
-fn are_aligned(busses: &Vec<BusLine>) -> bool {
-    let n: u64 = busses.get(0).unwrap().adj_timestamp();
-    busses.iter().all(|bus| bus.adj_timestamp() == n)
 }
 
 #[cfg(test)]
 mod tests {
-    use super::second;
+    use super::{second, BusLine};
 
     #[test]
     fn test_part2_1() {
@@ -138,5 +103,12 @@ mod tests {
     #[test]
     fn test_part2_3() {
         assert_eq!("1202161486", second("1789,37,47,1889".to_string()));
+    }
+
+    #[test]
+    fn test_departs_at() {
+        assert!(BusLine::from(0, "17").unwrap().departs_at(3417));
+        assert!(BusLine::from(2, "13").unwrap().departs_at(3419));
+        assert!(BusLine::from(3, "19").unwrap().departs_at(3420));
     }
 }

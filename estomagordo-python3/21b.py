8cca98da02370e7703fa2dfc2b278eb21b9666a7
Christofer Ohlsson <christofer.ohlsson@gmail.com>
2020-12-21 08:54:56 +0100

    cleaner 21b

--- estomagordo-python3/21b.py
+++ estomagordo-python3/21b.py
@@ -8,33 +8,25 @@ from helpers import distance, distance_sq, eight_neighs, eight_neighs_bounded, g
 
 
 def solve(lines):
-    ingredients = set()
-    allergens = {}
+    ingredients = reduce(lambda a,b: a|b, [line[0] for line in lines])
+    allergens = {allergen: set(ingredients) for allergen in reduce(lambda a,b: a|b, [line[1] for line in lines])}
 
-    for i, a in lines:
-        for ing in i:
-            ingredients.add(ing)
-
-    for i, a in lines:
-        for all in a:
-            allergens[all] = set(ingredients)
-
-    for i, a in lines:
-        for all in a:
-            allergens[all] &= i
+    for ing, all in lines:
+        for allergen in all:
+            allergens[allergen] &= ing
 
     definite = set()
 
-    for setty in allergens.values():
-        if len(setty) == 1:
-            definite |= setty
+    for ingredientset in allergens.values():
+        if len(ingredientset) == 1:
+            definite |= ingredientset
 
     while True:
         reductdict = {}
         
-        for key, setty in allergens.items():
-            if len(setty - definite) == 1:
-                reductdict[key] = setty - definite
+        for key, ingredientset in allergens.items():
+            if len(ingredientset - definite) == 1:
+                reductdict[key] = ingredientset - definite
 
         if not reductdict:
             break
@@ -43,24 +35,7 @@ def solve(lines):
             allergens[k] = v
             definite |= v
 
-    out = [(k, v) for k,v in allergens.items()]
-    out.sort()
-
-    return ','.join(list(pair[1])[0] for pair in out)
-    
-    targets = set()
-
-    for v in allergens.values():
-        targets |= v
-
-    count = 0
-
-    for i, _ in lines:
-        for ing in i:
-            if ing not in targets:
-                count += 1
-
-    return count
+    return ','.join(list(pair[1])[0] for pair in sorted((k, v) for k,v in allergens.items()))
 
 if __name__ == '__main__':
     lines = []
@@ -69,14 +44,14 @@ if __name__ == '__main__':
         for line in f.readlines():
             words = line.split()
             ingredients = set()
-            allergens = []
+            allergens = set()
             ingredone = False
 
             for word in words:
                 if word[0] == '(':
                     ingredone = True
                 elif ingredone:
-                    allergens.append(word[:-1])
+                    allergens.add(word[:-1])
                 else:
                     ingredients.add(word)
 

f7c51b950977bef307e6b7442fb9db00acbba36c
FiskAxel <75782069+FiskAxel@users.noreply.github.com>
2021-02-24 10:28:06 +0100

    Day10 & Day19

---
+++ FiskAxel-CSharp/Day10/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+
+namespace Day10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] puzzleInput = File.ReadAllLines("../../../puzzleInput10.txt");
+            int[] numbers = new int[puzzleInput.Length];
+            for (int i = 0; i < puzzleInput.Length; i++)
+            {
+                numbers[i] = int.Parse(puzzleInput[i]);
+            }
+            Array.Sort(numbers, 0, numbers.Length);
+
+            ////
+            //// PART 1
+            ////
+
+            int ones = 1; // From power outlet.
+            int threes = 1; // To device.
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                int dif = 0;
+                dif = numbers[i + 1] - numbers[i];
+                if (dif == 1)
+                {
+                    ones++;
+                }
+                else if (dif == 3)
+                {
+                    threes++;
+                }
+            }
+            Console.WriteLine("Part 1");
+            Console.WriteLine($"NumOfOnes: {ones} * NumOfThrees: {threes}. Equals: {ones * threes}\n");
+
+
+            ////
+            //// PART 2
+            ////
+
+            int[] numbers2 = new int[numbers.Length + 1];
+            numbers2[0] = 0; // Outlet.
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers2[i + 1] = numbers[i];
+            }
+            long[] ways = new long[numbers2.Length];
+            ways[ways.Length - 1] = 1;
+            for (int i = ways.Length - 2; i >= 0; i--)
+            {
+                if (numbers2[i] + 3 >= numbers2[i + 1])
+                {
+                    ways[i] += ways[i + 1];
+                }
+                if (i + 2 < numbers2.Length && numbers2[i] + 3 >= numbers2[i + 2])
+                {
+                    ways[i] += ways[i + 2];
+                }
+                if (i + 3 < numbers2.Length && numbers2[i] + 3 >= numbers2[i + 3])
+                {
+                    ways[i] += ways[i + 3];
+                }
+            }
+
+            Console.WriteLine("Part 2");
+            Console.WriteLine(ways[0]);
+        }
+    }
+}
---
+++ FiskAxel-CSharp/Day19/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Day19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //// INPUT PARSING
+
+            string[] puzzleInput = File.ReadAllLines("../../../puzzleInput19.txt");
+
+            int len = Array.IndexOf(puzzleInput, "");
+            string[] rules = new string[len];
+            for (int i = 0; i < len; i++)
+            {
+                string[] parse = puzzleInput[i].Split(": ");
+                int index = int.Parse(parse[0]);
+                rules[index] = parse[1];
+            }
+
+            int len2 = puzzleInput.Length - (len + 1);
+            string[] messages = new string[len2];
+            for (int i = 0; i < len2; i++)
+            {
+                messages[i] = puzzleInput[len + 1 + i];
+            }
+
+            ////
+            //// PART 1
+            ////
+
+            int result = 0;
+            Regex pattern1 = new Regex("^" + CreatePattern(rules, rules[0], 0, 0) + "$");
+            foreach (var message in messages)
+            {
+                if (pattern1.IsMatch(message))
+                {
+                    result++;
+                }
+            }
+
+            Console.WriteLine("Part 1: ");
+            Console.WriteLine(result);
+            
+            ////
+            //// PART 2
+            ////
+
+            rules[8] = "42 | 42 8";
+            rules[11] = "42 31 | 42 11 31";
+
+            result = 0;
+            Regex pattern2 = new Regex("^" + CreatePattern(rules, rules[0], 0, 0) + "$");
+            foreach (var message in messages)
+            {
+                if (pattern2.IsMatch(message))
+                {
+                    result++;
+                }
+            }
+
+            Console.WriteLine("Part 2: ");
+            Console.WriteLine(result);
+        }
+
+        static string CreatePattern(string[] rules, string rule, int len8, int len11)
+        {
+            if (rule == rules[8])
+            {
+                len8++;
+                if (len8 > 10)
+                {
+                    rule = "42";
+                } 
+            }
+            else if (rule == rules[11])
+            {
+                len11++;
+                if (len11 > 10)
+                {
+                    rule = "42 31";
+                }  
+            }
+            else if (rule[0] == '\"')
+            {
+                return rule.Substring(1,1);
+            }
+
+            string regPattern = "(";
+            string[] parsed = rule.Split(" ");
+            foreach (var item in parsed)
+            {
+                if (item == "|")
+                {
+                    regPattern += '|';
+                }
+                else
+                {
+                    regPattern += CreatePattern(rules, rules[int.Parse(item)], len8, len11);
+                }
+            }
+            regPattern += ')';
+
+            return regPattern;
+        }
+    } 
+}

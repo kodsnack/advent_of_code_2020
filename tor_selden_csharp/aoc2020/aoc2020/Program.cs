ac92c460c66a5a7fa14b98a1f11dc92f19a79a66
torselden <tor.selden@gmail.com>
2021-03-13 13:43:49 +0100

    (torselden) First commit and day1 in c#

---
+++ tor_selden_csharp/aoc2020/aoc2020/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace aoc2020
+{
+    class Program
+    {
+        public static string basePath = @"C:\git\advent_of_code_2020\tor_selden_csharp\aoc2020\aoc2020\input";
+
+        static void Main(string[] args)
+        {
+            //Day1.A();
+            Day1.B();            
+        }
+    }
+}
---
+++ tor_selden_csharp/aoc2020/aoc2020/Utils.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace aoc2020
+{
+    class Utils
+    {
+        public static IEnumerable<string> ReadInputLines(string input) 
+        {
+            return File.ReadAllLines(Path.Combine(Program.basePath, input));            
+        }
+    }
+}

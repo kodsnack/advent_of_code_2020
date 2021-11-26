c96887b73ab1ee09e01616127b3103e9cfbab9ac
jofafrazze <johan@hallonbrax.com>
2021-11-26 15:03:56 +0100

    Day 23, 24, 25

--- jofafrazze-csharp/day23/Day23.cs
+++ jofafrazze-csharp/day23/Day23.cs
@@ -2,12 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-
 using AdventOfCode;
-//using Position = AdventOfCode.GenericPosition2D<int>;
 
 namespace day23
 {
@@ -16,19 +11,84 @@ namespace day23
         readonly static string nsname = typeof(Day23).Namespace;
         readonly static string inputPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\..\" + nsname + "\\input.txt");
 
-        // Day 23: 
+        // Day 23: Crab Cups - Small game with numbers moving in a circle
+
+        static void StepCrabCups(LinkedList<int> cups, List<LinkedListNode<int>> valueToCup, int iter)
+        {
+            int maxValue = cups.Count;
+            LinkedListNode<int> curCup = cups.First;
+            for (int i = 0; i < iter; i++)
+            {
+                List<int> takeValues = new List<int>();
+                var nextCup = curCup.NextOrFirst();
+                for (int n = 0; n < 3; n++)
+                {
+                    var cup = nextCup;
+                    nextCup = nextCup.NextOrFirst();
+                    takeValues.Add(cup.Value);
+                }
+                int a = curCup.Value - 1;
+                while (a < 1 || takeValues.Contains(a))
+                    a = a < 1 ? maxValue : a - 1;
+                var destCup = valueToCup[a];
+                for (int n = 2; n >= 0; n--)
+                {
+                    var tCup = valueToCup[takeValues[n]];
+                    cups.Remove(tCup);
+                    cups.AddAfter(destCup, tCup);
+                }
+                curCup = curCup.NextOrFirst();
+            }
+        }
 
         static Object PartA()
         {
-            var input = ReadIndata.Ints(inputPath);
-            int ans = 0;
+            var input = ReadIndata.Strings(inputPath);
+            LinkedList<int> circularBuffer = new LinkedList<int>(input[0].Select(x => (int)x - '0'));
+            int n = circularBuffer.Count;
+            var valueToCup = new List<LinkedListNode<int>>(n + 1);
+            for (int i = 0; i <= n; i++)
+                valueToCup.Add(null);
+            var pos = circularBuffer.First;
+            for (int i = 1; i <= n; i++)
+            {
+                valueToCup[pos.Value] = pos;
+                pos = pos.Next;
+            }
+            StepCrabCups(circularBuffer, valueToCup, 100);
+            pos = circularBuffer.Find(1);
+            var pp = pos.NextOrFirst();
+            string ans = "";
+            while (pp != pos)
+            {
+                ans += pp.Value.ToString();
+                pp = pp.NextOrFirst();
+            }
             Console.WriteLine("Part A: Result is {0}", ans);
             return ans;
         }
 
         static Object PartB()
         {
-            int ans = 0;
+            var input = ReadIndata.Strings(inputPath);
+            LinkedList<int> circularBuffer = new LinkedList<int>(input[0].Select(x => (int)x - '0'));
+            int n = 1_000_000;
+            for (int i = 10; i <= n; i++)
+                circularBuffer.AddLast(i);
+            var valueToCup = new List<LinkedListNode<int>>(n + 1);
+            for (int i = 0; i <= n; i++)
+                valueToCup.Add(null);
+            var pos = circularBuffer.First;
+            for (int i = 1; i <= n; i++)
+            {
+                valueToCup[pos.Value] = pos;
+                pos = pos.Next;
+            }
+            StepCrabCups(circularBuffer, valueToCup, 10_000_000);
+            pos = circularBuffer.Find(1);
+            var p = pos.NextOrFirst();
+            var pp = p.NextOrFirst();
+            long ans = p.Value * (long)pp.Value;
             Console.WriteLine("Part B: Result is {0}", ans);
             return ans;
         }
@@ -45,8 +105,8 @@ namespace day23
 
         public static bool MainTest()
         {
-            int a = 42;
-            int b = 4711;
+            string a = "47382659";
+            long b = 42271866720;
             return (PartA().Equals(a)) && (PartB().Equals(b));
         }
     }
--- jofafrazze-csharp/day24/Day24.cs
+++ jofafrazze-csharp/day24/Day24.cs
@@ -2,12 +2,9 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-
 using AdventOfCode;
-//using Position = AdventOfCode.GenericPosition2D<int>;
+using Position = AdventOfCode.GenericPosition2D<int>;
 
 namespace day24
 {
@@ -16,19 +13,59 @@ namespace day24
         readonly static string nsname = typeof(Day24).Namespace;
         readonly static string inputPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\..\" + nsname + "\\input.txt");
 
-        // Day 24: 
+        // Day 24: Lobby Layout - Parse without delims, handle hex pattern
+
+        static HashSet<Position> black = new HashSet<Position>();
 
         static Object PartA()
         {
-            var input = ReadIndata.Ints(inputPath);
-            int ans = 0;
+            var input = ReadIndata.Strings(inputPath);
+            Regex re = new Regex(@"(e|se|sw|w|nw|ne)");
+            foreach (var s in input)
+            {
+                var pos = new Position();
+                foreach (Match match in re.Matches(s))
+                    pos += CoordsHex.directionsWide[match.Value];
+                if (black.Contains(pos))
+                    black.Remove(pos);
+                else
+                    black.Add(pos);
+            }
+            int ans = black.Count();
             Console.WriteLine("Part A: Result is {0}", ans);
             return ans;
         }
 
+        static HashSet<Position> GetNeighbours(Position p)
+        {
+            var m = new HashSet<Position>();
+            foreach (var d in CoordsHex.directionsWide.Values)
+                m.Add(p + d);
+            return m;
+        }
+
         static Object PartB()
         {
-            int ans = 0;
+            for (int i = 0; i < 100; i++)
+            {
+                var nextBlack = new HashSet<Position>(black);
+                var mNeigh = new HashSet<Position>(black);
+                foreach (var p in black)
+                    mNeigh.UnionWith(GetNeighbours(p));
+                foreach (var p in mNeigh)
+                {
+                    int n = 0;
+                    foreach (var d in CoordsHex.directionsWide.Values)
+                        if (black.Contains(p + d))
+                            n++;
+                    if (black.Contains(p) && (n == 0 || n > 2))
+                        nextBlack.Remove(p);
+                    else if (!black.Contains(p) && n == 2)
+                        nextBlack.Add(p);
+                }
+                black = nextBlack;
+            }
+            int ans = black.Count();
             Console.WriteLine("Part B: Result is {0}", ans);
             return ans;
         }
@@ -45,8 +82,8 @@ namespace day24
 
         public static bool MainTest()
         {
-            int a = 42;
-            int b = 4711;
+            int a = 523;
+            int b = 4225;
             return (PartA().Equals(a)) && (PartB().Equals(b));
         }
     }
